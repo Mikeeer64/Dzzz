1b1c0ffea9e4060f5e7ea765106061a5f2d2e641
Mikeeer <my email>
2022-12-12 19:43:10 +0300

    3

---
+++ Example03/Program.cs
@@ -0,0 +1,34 @@
+﻿int [] initArray(int length)
+{
+    int [] array = new int [length];
+    Random asd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = asd.Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SumArray(int[] arr)
+{
+    int summ = 0;
+    if (arr[0] > arr[4])
+        summ = arr[0] - arr[4];
+    else
+        summ = arr[4] - arr[0];
+    
+    Console.WriteLine(summ);
+}
+int [] array = initArray(5);
+PrintArray(array);
+SumArray(array);
+
